0041eeb78a53e04dc794d16db3c8da66331d7c91
Igor Senkiv <igor27031991@gmail.com>
2023-02-20 19:57:52 +0100

    Made some changest to web cluster+

--- live/stage/services/webserver-cluster/main.tf
+++ live/stage/services/webserver-cluster/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 module "webserver-cluster" {
-  source = "../../../modules/services/webserver-cluster"
+  source = "github.com/www/modules//services/webserver-cluster"
 
   cluster_name = "webservers-stage"
   db_remote_state_bucket = "stage-mysql-t-remote-state"
@@ -18,7 +18,7 @@ module "webserver-cluster" {
 # Expose an extra PORT in just the staging env for testing
 resource "aws_security_group_rule" "allow_testing_inbound" {
   type = "ingress"
-  security_group_id = aws_security_group.alb.id
+  security_group_id = module.webserver_cluster.alb_security_group_id
 
   from_port = 12345
   to_port = 122345
